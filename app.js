fc4d8acdec65e291b50ca628f0f759b053f1f3a2
PetterC <petterchan89@gmail.com>
2016-12-07 13:22:22 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-var yourFirstName;
-var yourLastName;
-var currentPersonInfo;
-
-function test()
-{
-	alert("Hello World");
-}
-
-/*var dataObject = {
-	  personId : 00001;
+var dataObject = [
 		 {
+		"Id" : 00001,
 	  "firstName": "Billy",
 	  "lastName": "Bob",
 	  "gender": "male",
@@ -21,8 +12,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00002
 	},
-	  personId = 00002;
 		 {
+		 "Id" : 00002,
 	  "firstName": "Uma",
 	  "lastName": "Bob",
 	  "gender": "female",
@@ -34,8 +25,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00001
 	},
-	  personId = 00003;
 	{
+	  "Id" : 00003,
 	  "firstName": "Michael",
 	  "lastName": "Walkens",
 	  "gender": "male",
@@ -47,8 +38,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00004
 	},
-	   personId = 00004;
 		 	{
+		"Id" : 00004,
 	  "firstName": "Jon",
 	  "lastName": "Walkens",
 	  "gender": "male",
@@ -60,8 +51,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00003
 	},
-	personId = 00005;
 	{
+	  "Id" : 00005,
 	  "firstName": "Jack",
 	  "lastName": "Pafoy",
 	  "gender": "male",
@@ -73,8 +64,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00006
 	},
-	  personId = 00006;
 	 {
+	  "Id" : 00006,
 	  "firstName": "Jen",
 	  "lastName": "Pafoy",
 	  "gender": "female",
@@ -86,8 +77,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00005
 	},
-	personId = 00007;
 	{
+	  "Id" : 00007,
 	  "firstName": "Mister",
 	  "lastName": "Potatoo",
 	  "gender": "male",
@@ -99,8 +90,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00008
 	},
-	personId = 00008;
 	{
+	  "Id" : 00008,
 	  "firstName": "Missuz",
 	  "lastName": "Potatoo",
 	  "gender": "female",
@@ -112,8 +103,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": 00007
 	},
-	personId = 00009;
 	{
+	  "Id" : 00009,
 	  "firstName": "Joy",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -125,8 +116,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": null
 	},
-	personId = 00010;
 	{
+	  "Id" : 00010,
 	  "firstName": "Mader",
 	  "lastName": "Madden",
 	  "gender": "male",
@@ -138,8 +129,8 @@ function test()
 	  "parents": [],
 	  "currentSpouse": null
 	},
-	personId = 00011;
 	{
+	  "Id" : 00011,
 	  "firstName": "Jill",
 	  "lastName": "Pafoy",
 	  "gender": "female",
@@ -149,10 +140,10 @@ function test()
 	  "eyeColor": "brown",
 	  "occupation": "programmer",
 	  "parents": [00006],
-	  "currentSpouse":
+	  "currentSpouse": null
 	},
-	 personId = 00012;
 	{
+	  "Id" : 00012,
 	  "firstName": "Ralph",
 	  "lastName": "Bob",
 	  "gender": "male",
@@ -164,8 +155,8 @@ function test()
 	  "parents": [00002,00001],
 	  "currentSpouse": 00013
 	},
-	personId = 00013;
 	{
+	  "Id" : 00013,
 	  "firstName": "Jasmine",
 	  "lastName": "Bob",
 	  "gender": "female",
@@ -177,8 +168,8 @@ function test()
 	  "parents": [00004,00003],
 	  "currentSpouse": 000012
 	},
-	personId = 00014;
 	{
+	  "Id" : 00014,
 	  "firstName": "Annie",
 	  "lastName": "Pafoy",
 	  "gender": "female",
@@ -190,8 +181,8 @@ function test()
 	  "parents": [00005,00006],
 	  "currentSpouse": null
 	},
-	personId = 00015;
 	{
+	  "Id" : 00015,
 	  "firstName": "Dave",
 	  "lastName": "Pafoy",
 	  "gender": "male",
@@ -203,8 +194,8 @@ function test()
 	  "parents": [00005,00006],
 	  "currentSpouse": 00011
 	},
-	personId = 00016;
 	{
+	  "Id" : 00016,
 	  "firstName": "Amii",
 	  "lastName": "Pafoy",
 	  "gender": "female",
@@ -216,8 +207,8 @@ function test()
 	  "parents": [00005,00006],
 	  "currentSpouse": null
 	},
-	personId = 00017;
 	{
+  	"Id" : 00017,
 	  "firstName": "Regina",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -229,8 +220,8 @@ function test()
 	  "parents": [00009, 00010],
 	  "currentSpouse": null
 	},
-personId = 00018;
 	{
+	  "Id" : 00018,
 	  "firstName": "Hana",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -242,8 +233,8 @@ personId = 00018;
 	  "parents": [00009, 00010],
 	  "currentSpouse": null
 	},
-	personId = 00019;
 	{
+	  "Id" : 00019,
 	  "firstName": "Eloise",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -255,8 +246,8 @@ personId = 00018;
 	  "parents": [00009, 00010],
 	  "currentSpouse": null
 	},
-	personId = 00020;
 	{
+	  "Id" : 00020,
 	  "firstName": "Mattias",
 	  "lastName": "Madden",
 	  "gender": "male",
@@ -268,8 +259,8 @@ personId = 00018;
 	  "parents": [00009, 00010],
 	  "currentSpouse": 00021
 	},
-personId = 00021;
 	{
+	  "Id" : 00021,
 	  "firstName": "Ellen",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -281,8 +272,8 @@ personId = 00021;
 	  "parents": [],
 	  "currentSpouse": 00020
 	},
-	personId = 00022;
 	{
+	  "Id" : 00022,
 	  "firstName": "Joey",
 	  "lastName": "Madden",
 	  "gender": "female",
@@ -294,343 +285,121 @@ personId = 00021;
 	  "parents": [00021,00020],
 	  "currentSpouse": null
 	}
-};*/
+];
 
-var dataObject = {
-	"272822514" : {
-	  "firstName": "Billy",
-	  "lastName": "Bob",
-	  "gender": "male",
-	  "dob": "1/18/1949",
-	  "height": 71,
-	  "weight": 175,
-	  "eyeColor": "brown",
-	  "occupation": "programmer",
-	  "parents": [],
-	  "currentSpouse": "Uma Bob",
-	},
-	"401222887" : {
-	  "firstName": "Uma",
-	  "lastName": "Bob",
-	  "gender": "female",
-	  "dob": "4/1/1947",
-	  "height": 65,
-	  "weight": 162,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": [],
-	  "currentSpouse": "Billy Bob",
-	},
-	"409574486" : {
-	  "firstName": "Michael",
-	  "lastName": "Walkens",
-	  "gender": "male",
-	  "dob": "5/9/1951",
-	  "height": 76,
-	  "weight": 250,
-	  "eyeColor": "brown",
-	  "occupation": "landscaper",
-	  "parents": [],
-	  "currentSpouse": "Jon Walkens",
-	},
-	"260451248" : {
-	  "firstName": "Jon",
-	  "lastName": "Walkens",
-	  "gender": "male",
-	  "dob": "9/6/1945",
-	  "height": 62,
-	  "weight": 115,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": [],
-	  "currentSpouse": "Michael Walkens",
-	},
-	"629807187" : {
-	  "firstName": "Jack",
-	  "lastName": "Pafoy",
-	  "gender": "male",
-	  "dob": "3/16/1938",
-	  "height": 70,
-	  "weight": 207,
-	  "eyeColor": "black",
-	  "occupation": "nurse",
-	  "parents": [],
-	  "currentSpouse": "Jen Pafoy",
-	},
-	"464142841" : {
-	  "firstName": "Jen",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "4/10/1940",
-	  "height": 72,
-	  "weight": 256,
-	  "eyeColor": "black",
-	  "occupation": "student",
-	  "parents": [],
-	  "currentSpouse": "Jack Pafoy",
-	},
-	"982411429" : {
-	  "firstName": "Mister",
-	  "lastName": "Potatoo",
-	  "gender": "male",
-	  "dob": "12/18/1952",
-	  "height": 66,
-	  "weight": 170,
-	  "eyeColor": "hazel",
-	  "occupation": "architect",
-	  "parents": [],
-	  "currentSpouse": "Missuz Potatoo",
-	},
-	"595767575" : {
-	  "firstName": "Missuz",
-	  "lastName": "Potatoo",
-	  "gender": "female",
-	  "dob": "10/28/1948",
-	  "height": 59,
-	  "weight": 137,
-	  "eyeColor": "blue",
-	  "occupation": "architect",
-	  "parents": [],
-	  "currentSpouse": "Mister Potatoo",
-	},
-	"693243224" : {
-	  "firstName": "Joy",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "4/20/1939",
-	  "height": 69,
-	  "weight": 199,
-	  "eyeColor": "hazel",
-	  "occupation": "doctor",
-	  "parents": [],
-	  "currentSpouse": null
-	},
-	"888201200" : {
-	  "firstName": "Mader",
-	  "lastName": "Madden",
-	  "gender": "male",
-	  "dob": "5/6/1937",
-	  "height": 76,
-	  "weight": 205,
-	  "eyeColor": "black",
-	  "occupation": "landscaper",
-	  "parents": [],
-	  "currentSpouse": null
-	},
-	"878013758" : {
-	  "firstName": "Jill",
-	  "lastName": "Pafoy",
-	  "gender": "Bob",
-	  "dob": "2/8/1972",
-	  "height": 74,
-	  "weight": 118,
-	  "eyeColor": "brown",
-	  "occupation": "programmer",
-	  "parents": "Uma Bob",
-	  "currentSpouse": "Dave Pafoy",
-	},
-	"951747547" : {
-	  "firstName": "Ralph",
-	  "lastName": "Bob",
-	  "gender": "male",
-	  "dob": "12/23/1969",
-	  "height": 66,
-	  "weight": 179,
-	  "eyeColor": "blue",
-	  "occupation": "nurse",
-	  "parents": "Uma Bob",
-	  "currentSpouse": "Jasmine Bob",
-	},
-	"159819275" : {
-	  "firstName": "Jasmine",
-	  "lastName": "Bob",
-	  "gender": "female",
-	  "dob": "12/18/1969",
-	  "height": 58,
-	  "weight": 156,
-	  "eyeColor": "blue",
-	  "occupation": "assistant",
-	  "parents": ["Michael Walkens","Jon Walkens"],
-	  "currentSpouse": "Ralph Bob",
-	},
-	"348457184" : {
-	  "firstName": "Annie",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "11/4/1970",
-	  "height": 62,
-	  "weight": 235,
-	  "eyeColor": "hazel",
-	  "occupation": "landscaper",
-	  "parents": ["Jack Pafoy","Jen Pafoy"],
-	  "currentSpouse": null
-	},
-	"294874671" : {
-	  "firstName": "Dave",
-	  "lastName": "Pafoy",
-	  "gender": "male",
-	  "dob": "8/5/1967",
-	  "height": 61,
-	  "weight": 112,
-	  "eyeColor": "green",
-	  "occupation": "doctor",
-	  "parents": ["Jack Pafoy","Jen Pafoy"],
-	  "currentSpouse": "Jill Pafoy"
-	},
-	"931247228" : {
-	  "firstName": "Amii",
-	  "lastName": "Pafoy",
-	  "gender": "female",
-	  "dob": "3/13/1963",
-	  "height": 74,
-	  "weight": 184,
-	  "eyeColor": "brown",
-	  "occupation": "landscaper",
-	  "parents": ["Jack Pafoy","Jen Pafoy"],
-	  "currentSpouse": null
-	},
-	"822843554" : {
-	  "firstName": "Regina",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "7/26/1959",
-	  "height": 71,
-	  "weight": 249,
-	  "eyeColor": "brown",
-	  "occupation": "nurse",
-	  "parents": ["Joy Madden", "Mader Madden"],
-	  "currentSpouse": null
-	},
-	"819168108" : {
-	  "firstName": "Hana",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "10/7/1953",
-	  "height": 70,
-	  "weight": 187,
-	  "eyeColor": "brown",
-	  "occupation": "politician",
-	  "parents": ["Joy Madden", "Mader Madden"],
-	  "currentSpouse": null
-	},
-	"969837479" : {
-	  "firstName": "Eloise",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "12/11/1961",
-	  "height": 63,
-	  "weight": 241,
-	  "eyeColor": "brown",
-	  "occupation": "assistant",
-	  "parents": ["Joy Madden", "Mader Madden"],
-	  "currentSpouse": null
-	},
-	"313207561" : {
-	  "firstName": "Mattias",
-	  "lastName": "Madden",
-	  "gender": "male",
-	  "dob": "2/19/1966",
-	  "height": 70,
-	  "weight": 110,
-	  "eyeColor": "blue",
-	  "occupation": "assistant",
-	  "parents": ["Joy Madden", "Mader Madden"],
-	  "currentSpouse": "Ellen Madden",
-	},
-	"313997561" : {
-	  "firstName": "Ellen",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "2/19/1970",
-	  "height": 67,
-	  "weight": 100,
-	  "eyeColor": "blue",
-	  "occupation": "doctor",
-	  "parents": [],
-	  "currentSpouse": "Mattias Madden",
-	},
-	"313998000" : {
-	  "firstName": "Joey",
-	  "lastName": "Madden",
-	  "gender": "female",
-	  "dob": "2/02/1987",
-	  "height": 67,
-	  "weight": 100,
-	  "eyeColor": "blue",
-	  "occupation": "doctor",
-	  "parents": ["Mattias Madden","Ellen Madden"],
-	  "currentSpouse": null
-	}
-};
-
-
-/*  Remove this from your final submission
-function printAllToConsole(dataObj){
-	for (var key in dataObj) {
-		if (dataObj.hasOwnProperty(key)) {
-			console.log(key + " -> " + JSON.stringify(dataObj[key]));
-		}
-	}
-}
-printAllToConsole(dataObject);
-*/
+initSearch();
 
 function initSearch()
 {
 	alert("Hello World");
 
-	// get all the information you need to run the search
 	yourFirstName = prompt("First names");
 
     yourLastName = prompt("Last Name");
-	// then pass that info to the respective function.
-	//currentPersonInfo = getPersonInfo(yourFirstName, yourLastName)
 
-	var person;
-    for (person in dataObject)
-	{
-    	if (dataObject[person].firstName == yourFirstName)
-		{
-			getPersonInfo(person);
-		}
-	}
-	// once the search is done, pass the results to the responder function
-	//responder(result);
+var result = getPersonInfo(yourFirstName, yourLastName)
+console.log(result)
+
+var result2 = getChildOf(result[0]);
+console.log(result2);
+
+var result3 = getDescendantsOf(result[0]);
+console.log(result3);
+
+/*var result4 = getFamily(result)
+console.log(result4);*/
+
+var result5 = objectToArray(result[0]);
+console.log(result4);
+
+var result6 = isMatch(result[0]);
+console.log(result5);
+
+
+
 }
 
-/*function responder(results){
-	// results may be a list of strings, an object, or a single string.
+function responder(results){
+
 	alert(results);
+}
+function getPersonInfo(firstname, lastname){
+
+ return dataObject.filter(function(el){
+	 return (el.firstName==firstname && el.lastName == lastname);
+});
+}
+
+function getDescendantsOf(getChildOf, person) {
+
+	getChildOf = [0]
+  if (getChildOf[0]==person){
+	return true;
+} else {
+return getDescendantsOf(person[0]) + getDescendantsOf(person[1]);
+}
+	};
+
+	/*function getFamily(person) {
+   var member = [0];
+	 if (member[0]==person || member[1]==person){
+	 return true;
+ } else {
+ return getFamily(person[0]) + getFamily(person][1]);
+}
+	};*/
+
+function getChildOf(person){
+	return dataObject.filter(function(el){
+		return(el.parents[0]==person.Id || el.parents[1]==person.Id);
+	});
+	}
+
+
+	function objectToArray(dataObject) {
+	    var objects = [];
+	    for (key in dataObject) {
+	        objects.push(dataObject[key]);
+	    }
+	    return objects;
+	}
+
+	function isMatch(properties, person)
+	{
+	    var propertyArray = properties.split(",");
+	    var propDict = [];
+	    propertyArray.forEach(function(entry)
+	    {
+	        var temparry = entry.split(':');
+	        propDict[temparry[0]] = temparry[1];
+	    });
+	    if(!person.eyeColor == propDict["eyeColor"]) //Add check inside parenthesis to make sure dictionary has that key  if(keyDict has eye color key && !person.eyeColor == propDict["eyeColor"])
+	        return false;
+	    // this is will be all IFs for the altributes for the person...if the attribute is not a search parameter, the if will fall through to the next if
+
+	    return true;
+	}
+
+	/*function initSearch()
+	{
+	    var objects = objectToArray(dataObject);
+	    searchParams = prompt("SearchParams");   //validate search params to make sure it matches the format...  eyeColor:blue,asdfjalsfd:brown
+	    var results1 = objects.filter(function(person){
+	        return (isMatch(searchParams,person));
+	    }).map(function(person){
+	        alert(person.firstName + " " + person.lastName);  //instead of alerting, concatenate all of the names in a string to display.
+	    });
+	}
+
+
+
+/*function addSixTo(x){
+	return x + 6;
 }*/
 
-function getPersonInfo(personID)
-{
-	var uiEle = document.getElementById('fullName');
-	uiEle.value = dataObject[personID].firstName + " " + dataObject[personID].lastName;
-	uiEle = document.getElementById('gender');
-	uiEle.value = dataObject[personID].gender;
-	uiEle = document.getElementById('dob');
-	uiEle.value = dataObject[personID].dob;
-	uiEle = document.getElementById('height');
-	uiEle.value = dataObject[personID].height;
-	uiEle = document.getElementById('weight');
-	uiEle.value = dataObject[personID].weight;
-	uiEle = document.getElementById('eye');
-	uiEle.value = dataObject[personID].eyeColor;
-	uiEle = document.getElementById('occupation')
-	uiEle.value = dataObject[personID].occupation;
-	uiEle = document.getElementById('parents')
-	uiEle.value = dataObject[personID].parents;
-	uiEle = document.getElementById('Current Spouse')
-	uiEle.value = dataObject[personID].currentSpouse;
-}
 
-/*function getFamily(){
-	// return list of names of immediate family members
-}
 
-// there will be much more here, and some of the code above will certainly change
 
-initSearch();
-window.close(); // exit window as the end of the session -- you may remove this
-*/
+//initSearch();
+//window.close(); // exit window as the end of the session -- you may remove this
+//*/
